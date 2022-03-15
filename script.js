118f6f291343f72d86fe7b5e80ee96c12218c485
Twahid Uddin <Twahiduddin@aol.com>
2022-03-15 13:59:02 -0400

    reset code to work on different branch

--- script.js
+++ script.js
@@ -4,28 +4,8 @@ let cols = 0;
 
 //Adds a row
 function addR() {
-    //alert("Clicked Add Row")
-    let grid = document.getElementById("grid");
-    
-    if (rows === 0 || cols === 0) {
-        cols += 1;
-    };
-    
-    let row = document.createElement("tr");
-    rows += 1;
-
-    for (let i = 0; i < cols; i++){
-        let col = document.createElement("td");
-        col.onclick = function (){
-            this.style.backgroundColor = colorSelected;
-        };
-        row.appendChild(col);
-    }
-    grid.appendChild(row);
+   
 }
-
-    
-
 //Adds a column
 function addC() {
     //alert("Clicked Add Col")
